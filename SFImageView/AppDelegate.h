903f3b6ef19677f56369b8f02b5a3be98e96d5fe
SunFengFF <1617273756@qq.com>
2016-03-01 22:00:16 +0800

    Initial Commit

---
+++ SFImageView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SFImageView
+//
+//  Created by sunfeng on 16/3/1.
+//  Copyright © 2016年 Bever. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SFImageView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SFImageView
+//
+//  Created by sunfeng on 16/3/1.
+//  Copyright © 2016年 Bever. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
